28e36a0ebb4221650051db8503478ff08671db62
Jori Lindell <jori.lindell@gmail.com>
2020-04-28 18:11:38 +0300

    Implement PageWrapper component to set page title to each page

--- src/domain/landingPage/LandingPage.tsx
+++ src/domain/landingPage/LandingPage.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Container from '../app/layout/Container';
+import PageWrapper from '../app/layout/PageWrapper';
 
 const LandingPage = () => {
   const { t } = useTranslation();
   return (
-    <Container>
-      <h1>{t('appName')}</h1>
-    </Container>
+    <PageWrapper>
+      <Container>
+        <h1>{t('appName')}</h1>
+      </Container>
+    </PageWrapper>
   );
 };
 
