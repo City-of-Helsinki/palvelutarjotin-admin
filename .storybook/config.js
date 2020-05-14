a418bc9df3cda829961ae894442ca5e1ad58c4f5
Jori Lindell <jori.lindell@gmail.com>
2020-05-14 12:37:16 +0300

    Wrap all stories with I18nextProvider and add main styles to storybook

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { I18nextProvider } from 'react-i18next';
+
+import '../src/styles/main.scss';
+import 'hds-core/lib/base.css';
+import i18n from '../src/domain/app/i18n/i18nInit';
+
+addDecorator(story => <I18nextProvider i18n={i18n}>{story()}</I18nextProvider>);
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../src', true, /\.stories\.(ts|js|md)x$/), module);
