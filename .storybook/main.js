b4a44a36cbe28000bb46a449cd894291907d7ac2
Jori Lindell <jori.lindell@gmail.com>
2020-05-14 12:38:04 +0300

    Add a11y addon to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
+    "@storybook/addon-a11y",
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs',
