5f406b24d22093bdc92e886bbbb65c37ae4d8866
Jori Lindell <jori.lindell@gmail.com>
2020-05-26 09:03:44 +0300

    Replace enzyme with @testing-library on TextWithLineBreaks test

--- src/common/components/textWithLineBreaks/__tests__/TextWithLineBreaks.test.tsx
+++ src/common/components/textWithLineBreaks/__tests__/TextWithLineBreaks.test.tsx
@@ -1,11 +1,11 @@
-import { shallow } from 'enzyme';
-import * as React from 'react';
+import { render } from '@testing-library/react';
+import React from 'react';
 
 import TextWithLineBreaks from '../TextWithLineBreaks';
 
 it('TextWithLineBreaks matches snapshot', () => {
   const text = `Line 1
   Line 2`;
-  const component = shallow(<TextWithLineBreaks as="div" text={text} />);
-  expect(component.html()).toMatchSnapshot();
+  const { container } = render(<TextWithLineBreaks as="div" text={text} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
