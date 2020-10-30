2896f0001be37541e02984b815c299bc58151f62
Santeri Rusila <santeri.rusila@sarmadigital.com>
2020-10-30 10:18:51 +0200

    Delete static mock data from EventDetailsButtons

--- src/domain/event/eventDetailsButtons/__test__/EventDetailsButtons.test.tsx
+++ src/domain/event/eventDetailsButtons/__test__/EventDetailsButtons.test.tsx
@@ -3,9 +3,15 @@ import { axe } from 'jest-axe';
 import { advanceTo, clear } from 'jest-date-mock';
 import React from 'react';
 
-import eventData from '../__mocks__/eventData.json';
+import { fakeEvent } from '../../../../utils/mockDataUtils';
 import { render, screen } from '../../../../utils/testUtils';
 import EventDetailsButtons from '../EventDetailsButtons';
+
+const eventId = 'palvelutarjotin:afzunowba4';
+
+const event = fakeEvent({
+  id: eventId,
+});
 
 afterAll(() => {
   clear();
@@ -18,7 +24,7 @@ afterEach(() => {
 test('is accessible and matches snapshot', async () => {
   const { container } = render(
     <EventDetailsButtons
-      eventData={eventData as any}
+      eventData={{ event }}
       onClickLanguage={jest.fn()}
       selectedLanguage="fi"
     />
@@ -36,7 +42,7 @@ test('it renders correct texts and click events work', () => {
   const clickLanguageMock = jest.fn();
   const { history } = render(
     <EventDetailsButtons
-      eventData={eventData as any}
+      eventData={{ event }}
       onClickLanguage={clickLanguageMock}
       selectedLanguage="fi"
     />
@@ -55,7 +61,7 @@ test('it renders correct texts and click events work', () => {
 
   expect(pushSpy).toHaveBeenCalledTimes(2);
   expect(pushSpy).toHaveBeenCalledWith({
-    pathname: '/fi/events/palvelutarjotin:afzunowba4/edit',
+    pathname: `/fi/events/${eventId}/edit`,
     search: '?language=fi',
   });
 
