ba0abc83c0579c1a1f675e6313c47b0af3fa6aa8
Ivan Melnik <melnik.job@gmail.com>
2021-05-07 10:38:40 +0300

    init

--- src/domain/app/footer/Footer.tsx
+++ src/domain/app/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Koros } from 'hds-react';
 import React, { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { PRIVACY_POLICY_LINKS } from '../../../constants';
+import { PRIVACY_POLICY_LINKS, TEACHER_UI_LINKS } from '../../../constants';
 import useLocale from '../../../hooks/useLocale';
 import Container from '../layout/Container';
 import styles from './footer.module.scss';
@@ -19,14 +19,25 @@ const Footer: FunctionComponent = () => {
           <div className={styles.helsinkiLogo}></div>
           <div className={styles.copyright}>
             <div>{t('footer.copyrightText')}</div>
-            <div>
-              <a
-                href={PRIVACY_POLICY_LINKS[locale]}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('footer.privacyPolicy')}
-              </a>
+            <div className={styles.links}>
+              <div>
+                <a
+                  href={TEACHER_UI_LINKS[locale]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {t('footer.teacherUI')}
+                </a>
+              </div>
+              <div>
+                <a
+                  href={PRIVACY_POLICY_LINKS[locale]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {t('footer.privacyPolicy')}
+                </a>
+              </div>
             </div>
           </div>
         </div>
