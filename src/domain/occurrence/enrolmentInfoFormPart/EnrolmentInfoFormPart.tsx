75e96c053c9b185d3b73ae3b75987d04a8b6b45b
Niko Lindroos <niko.j.makela@gmail.com>
2021-09-13 16:11:48 +0300

    feat(enrolment-type): an enrolment type selector implemented.
    When creating a new event, on 2nd step of the form wizard in an enrolment section, you can now choose what type of enrolments are used with an event. Enrolment types are internal, external and unenrollable. Changing the selected type changes the visible field set in enrolments section.
    
    PT-849 PT-1177.

--- src/domain/occurrence/enrolmentInfoFormPart/EnrolmentInfoFormPart.tsx
+++ src/domain/occurrence/enrolmentInfoFormPart/EnrolmentInfoFormPart.tsx
@@ -1,4 +1,6 @@
 import { Field, useFormikContext } from 'formik';
+import { RadioButton, SelectionGroup } from 'hds-react';
+import capitalize from 'lodash/capitalize';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -8,8 +10,17 @@ import TextInputField from '../../../common/components/form/fields/TextInputFiel
 import styles from '../occurrencePage.module.scss';
 import { TimeAndLocationFormFields } from '../types';
 
+export enum EnrolmentType {
+  Internal = 'internal',
+  External = 'external',
+  Unenrollable = 'unenrollable',
+}
+
 const EnrolmentInfoFormPart: React.FC = () => {
   const { t } = useTranslation();
+  const [enrolmentType, setEnrolmentType] = React.useState<EnrolmentType>(
+    EnrolmentType.Internal
+  );
 
   const {
     values: { neededOccurrences, autoAcceptance },
@@ -24,73 +35,132 @@ const EnrolmentInfoFormPart: React.FC = () => {
     }
   }, [autoAcceptance, neededOccurrences, setFieldValue]);
 
+  const enrolmentFieldSetComponentByType = {
+    [EnrolmentType.Internal]: (
+      <InternalEnrolmentFields
+        neededOccurrences={neededOccurrences as number}
+      />
+    ),
+    [EnrolmentType.External]: <ExternalEnrolmentFields />,
+    [EnrolmentType.Unenrollable]: <div></div>,
+  };
+
   return (
     <div className={styles.formSection}>
       <div className={styles.formSectionInnerContainer}>
         <div>
           <h2>{t('eventForm.enrolment.title')}</h2>
-          {/* Uncommment these when API supports them :) */}
-          {/* <div className={styles.formRow}>
-            <Field
-              label={t('eventForm.enrolment.labelEnrolmentRequired')}
-              name="enrolmentNeeded"
-              component={CheckboxField}
-            />
-            <Field
-              label={t('eventForm.enrolment.labelExternalEnrolment')}
-              name="externalEnrolment"
-              component={CheckboxField}
-            />
-          </div>
-          <div className={styles.formRow}>
-            <div>
-              <Field
-                label={t('eventForm.enrolment.labelExternalEnrolmentUrl')}
-                name="externalEnrolmentUrl"
-                disabled={!externalEnrolment}
-                component={TextInputField}
-              />
-            </div>
-          </div> */}
-          <div className={styles.formRow}>
-            <Field
-              className={styles.enrolmentStartInput}
-              required
-              labelText={t('eventForm.basicInfo.labelEnrolmentStart')}
-              name="enrolmentStart"
-              component={DateInputField}
-              timeSelector={true}
-            />
-            <div>
-              <Field
-                required
-                label={t('eventForm.basicInfo.labelEnrolmentEndDays')}
-                name="enrolmentEndDays"
-                component={TextInputField}
-                min={0}
-                type="number"
-              />
-            </div>
-            <div>
-              <Field
-                label={t('eventForm.categorisation.labelNeededOccurrences')}
-                name="neededOccurrences"
-                required
-                component={TextInputField}
-                min={1}
-                type="number"
-              />
-            </div>
-          </div>
-          <div className={styles.formRow}>
-            <Field
-              label={t('eventOccurrenceForm.labelAutoAcceptance')}
-              name="autoAcceptance"
-              disabled={neededOccurrences > 1}
-              component={CheckboxField}
-            />
-          </div>
+          <EnrolmentTypeSelector
+            enrolmentType={enrolmentType}
+            setEnrolmentType={setEnrolmentType}
+          />
+          {enrolmentFieldSetComponentByType[enrolmentType]}
         </div>
+      </div>
+    </div>
+  );
+};
+
+export const EnrolmentTypeSelector: React.FC<{
+  enrolmentType: EnrolmentType;
+  setEnrolmentType: React.Dispatch<React.SetStateAction<EnrolmentType>>;
+}> = ({ enrolmentType, setEnrolmentType }) => {
+  const { t } = useTranslation();
+
+  const onChangeEnrolmentType = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnrolmentType(event.target.value as EnrolmentType);
+  };
+
+  const isActiveEnrolmentType = (type: EnrolmentType) => {
+    return enrolmentType === type;
+  };
+
+  return (
+    <SelectionGroup
+      direction="horizontal"
+      className={styles.enrolmentTypeSelector}
+    >
+      {Object.values(EnrolmentType).map((type) => (
+        <RadioButton
+          key={`enrolmentType-${type}`}
+          id={`enrolmentType-${type}`}
+          name="enrolmentType"
+          value={type}
+          label={t(`eventForm.enrolmentType.label${capitalize(type)}`)}
+          checked={isActiveEnrolmentType(type)}
+          onChange={onChangeEnrolmentType}
+          className={`${styles.enrolmentTypeSelectorItem} ${
+            isActiveEnrolmentType(type) &&
+            styles.enrolmentTypeSelectorItemActive
+          }`}
+        />
+      ))}
+    </SelectionGroup>
+  );
+};
+
+export const InternalEnrolmentFields: React.FC<{
+  neededOccurrences: number;
+}> = ({ neededOccurrences }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div className={styles.formRow}>
+        <Field
+          className={styles.enrolmentStartInput}
+          required
+          labelText={t('eventForm.basicInfo.labelEnrolmentStart')}
+          name="enrolmentStart"
+          component={DateInputField}
+          timeSelector={true}
+        />
+        <div>
+          <Field
+            required
+            label={t('eventForm.basicInfo.labelEnrolmentEndDays')}
+            name="enrolmentEndDays"
+            component={TextInputField}
+            min={0}
+            type="number"
+          />
+        </div>
+        <div>
+          <Field
+            label={t('eventForm.categorisation.labelNeededOccurrences')}
+            name="neededOccurrences"
+            required
+            component={TextInputField}
+            min={1}
+            type="number"
+          />
+        </div>
+      </div>
+      <div className={styles.formRow}>
+        <Field
+          label={t('eventOccurrenceForm.labelAutoAcceptance')}
+          name="autoAcceptance"
+          disabled={neededOccurrences > 1}
+          component={CheckboxField}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const ExternalEnrolmentFields: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div>
+        <Field
+          label={t('eventForm.enrolment.labelExternalEnrolmentUrl')}
+          name="externalEnrolmentUrl"
+          component={TextInputField}
+        />
       </div>
     </div>
   );
