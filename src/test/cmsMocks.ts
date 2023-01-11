1fa5f9bffb33b4261da705eaf4854d122d25058f
Janne Talvio <janne.talvio@capgemini.com>
2023-01-11 09:34:19 +0200

    Revert "fix: remove unnecessary broken cms header test"
    This reverts commit f0c5bf54b8ac1fbbeaae133f3e99f4639e1210b8.

--- src/test/cmsMocks.ts
+++ src/test/cmsMocks.ts
@@ -4,7 +4,28 @@ import { MenuQuery } from '../generated/graphql-cms';
 import { fakePage } from '../utils/cmsMockDataUtils';
 import { server } from './msw/server';
 
+const route1 = '/helsinki-liikkuu/';
+const route2 = '/helsinki-liikkuu/alisivu/';
+const title1 = 'Mikä Kultus?';
+const subTitle1 = 'Mikä Kultus alisivu1';
+const subTitle2 = 'Mikä Kultus alisivu2';
 const mainMenu = [
+  {
+    title: title1,
+    uri: route1,
+    children: [
+      {
+        title: subTitle1,
+        uri: route2,
+        slug: route2,
+      },
+      {
+        title: subTitle2,
+        uri: route2,
+        slug: route2,
+      },
+    ],
+  },
   { title: 'Helsinki liikkuu', uri: '/test1', slug: 'test1' },
   { title: 'Kulttuurikasvatus', uri: '/test2', slug: 'test2' },
   { title: 'Oppimateriaalit', uri: '/test3', slug: 'test3' },
