ab7b85823b29dbc058b8f9b478b4a4f04b3ba5af
Kari Salminen <kari.salminen@anders.com>
2024-03-17 13:41:39 +0200

    test: fix browser tests
    refs PT-1727

--- testcafe/e2e/changeLanguage.ts
+++ testcafe/e2e/changeLanguage.ts
@@ -7,6 +7,7 @@ import { getEnvUrl } from '../utils/settings';
 fixture('Landing page').page(getEnvUrl('fi'));
 
 test('Changing language on login page', async (t) => {
+  // Expect default language to be Finnish
   await t
     .expect(
       screen.getByRole('heading', { name: /tapahtumien hallinta/i }).exists
@@ -15,17 +16,37 @@ test('Changing language on login page', async (t) => {
     .expect(screen.getByRole('button', { name: /kirjaudu sisään/i }).exists)
     .ok();
 
-  await t
-    .click(header.languageSelector)
-    .click(header.languageSelectorItemSv)
-    .expect(getPathname())
-    .eql('/sv');
+  // Change language to Swedish
+  await t.click(header.languageSelectorItemSv).expect(getPathname()).eql('/sv');
 
+  // Check that language was changed to Swedish
   await t
     .expect(
       screen.getByRole('heading', { name: /hantering av evenemang/i }).exists
     )
     .ok()
     .expect(screen.getByRole('button', { name: /logga in/i }).exists)
+    .ok();
+
+  // Change language to English
+  await t.click(header.languageSelectorItemEn).expect(getPathname()).eql('/en');
+
+  // Check that language was changed to English
+  await t
+    .expect(screen.getByRole('heading', { name: /event management/i }).exists)
+    .ok()
+    .expect(screen.getByRole('button', { name: /log in/i }).exists)
+    .ok();
+
+  // Change language to Finnish
+  await t.click(header.languageSelectorItemFi).expect(getPathname()).eql('/fi');
+
+  // Check that language was changed to Finnish
+  await t
+    .expect(
+      screen.getByRole('heading', { name: /tapahtumien hallinta/i }).exists
+    )
+    .ok()
+    .expect(screen.getByRole('button', { name: /kirjaudu sisään/i }).exists)
     .ok();
 });
