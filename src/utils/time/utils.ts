b6d7adabb0da1a1805ac5e5a75e89c8c15e172b3
Niko Lindroos <niko.j.makela@gmail.com>
2025-04-08 14:47:54 +0300

    chore(deps): upgrade date-fns
    PT-1807.
    
    Upgrade date-fns to the latest version (v4).
    
    Fix time parsing after date-fns update.

--- src/utils/time/utils.ts
+++ src/utils/time/utils.ts
@@ -1,30 +1,24 @@
-import { isBefore, isValid as isValidDate, parse as parseDate } from 'date-fns';
+import { isBefore, isValid, parse } from 'date-fns';
 
 import { DATE_FORMAT, DATETIME_FORMAT, TIME_FORMAT } from './format';
 
-export const parseTimeString = (time: string) => {
-  return parseDate(time, TIME_FORMAT, new Date());
-};
+export const parseTimeString = (time: string) =>
+  parse(time, TIME_FORMAT, new Date());
 
-export const parseDateString = (time: string) => {
-  return parseDate(time, DATE_FORMAT, new Date());
-};
+export const parseDateString = (time: string) =>
+  parse(time, DATE_FORMAT, new Date());
 
-export const parseDateTimeString = (time: string) => {
-  return parseDate(time, DATETIME_FORMAT, new Date());
-};
+export const parseDateTimeString = (time: string) =>
+  parse(time, DATETIME_FORMAT, new Date());
 
-export const isValidTimeString = (time: string) => {
-  return isValidDate(parseTimeString(time));
-};
+export const isValidTimeString = (time: string) =>
+  !time ? true : isValid(parseTimeString(time));
 
-export const isValidDateString = (time: string) => {
-  return isValidDate(parseDateString(time));
-};
+export const isValidDateString = (time: string) =>
+  !time ? true : isValid(parseDateString(time));
 
-export const isValidDateTimeString = (time: string) => {
-  return isValidDate(parseDateTimeString(time));
-};
+export const isValidDateTimeString = (time: string) =>
+  !time ? true : isValid(parseDateTimeString(time));
 
 export const isTimeStringBefore = (a: string, b: string) => {
   const timeA = parseTimeString(a);
