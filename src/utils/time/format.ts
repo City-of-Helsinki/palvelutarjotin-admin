81734ce99717983203cd611480b5598424ec4013
Niko Lindroos <niko.j.makela@gmail.com>
2025-04-08 15:31:55 +0300

    feat: fix timezone to Helsinki's zone
    PT-1901 PT-1902.
    
    Installed @date-fns/tz to fix the timezone.

--- src/utils/time/format.ts
+++ src/utils/time/format.ts
@@ -1,3 +1,4 @@
+import { tz } from '@date-fns/tz';
 import { format as formatDate } from 'date-fns';
 
 import type { Language } from '../../types';
@@ -10,23 +11,46 @@ const locales = { en, fi, sv };
 export const DATE_FORMAT = 'd.M.yyyy';
 export const TIME_FORMAT = 'HH:mm';
 export const DATETIME_FORMAT = `${DATE_FORMAT} ${TIME_FORMAT}`;
+export const TIMEZONE = 'Europe/Helsinki';
 
+/**
+ * Formats a Date object into a time string in the specified timezone.
+ */
 export function formatIntoTime(date: Date): string {
-  return formatDate(date, TIME_FORMAT);
+  return formatDate(date, TIME_FORMAT, {
+    in: tz(TIMEZONE),
+  });
 }
 
+/**
+ * Formats a Date object into a datetime string in the specified timezone.
+ */
 export function formatIntoDateTime(date: Date): string {
-  return formatDate(date, DATETIME_FORMAT);
+  return formatDate(date, DATETIME_FORMAT, {
+    in: tz(TIMEZONE),
+  });
 }
 
+/**
+ * Formats a Date object into a date string in the specified timezone.
+ */
 export function formatIntoDate(date: Date): string {
-  return formatDate(date, DATE_FORMAT);
+  return formatDate(date, DATE_FORMAT, {
+    in: tz(TIMEZONE),
+  });
 }
 
-export function formatDateRange(start: Date, end: Date) {
+/**
+ * Formats a start and end Date object into a date range string in the specified timezone.
+ */
+export function formatDateRange(start: Date, end: Date): string {
   return `${formatIntoDate(start)} – ${formatIntoDate(end)}`;
 }
 
+/**
+ * Formats a Date object into a localized date string based on the provided format and locale.
+ * If the date is null, an empty string is returned.
+ */
 export function formatLocalizedDate(
   date: Date | null,
   format = DATE_FORMAT,
@@ -35,8 +59,8 @@ export function formatLocalizedDate(
   if (!date) {
     return '';
   }
-
   return formatDate(date, format, {
     locale: locales[locale],
+    in: tz(TIMEZONE),
   });
 }
