2ca7d938249bf8a55d91e4b4da335ce92a29257f
Kari Salminen <kari.salminen@anders.com>
2025-04-10 14:30:50 +0300

    chore: update `*` using routes for react-router v7 compatibility
    Update routes and enable v7_relativeSplatPath future flag for
    react-router based on:
    - https://reactrouter.com/upgrading/v6#v7_relativesplatpath
    
    Add explicit index route for /fi/, /sv/ and /en/ to AppRoutes to get rid
    of warnings when running tests:
    ```
    stderr | src/domain/app/routes/__tests__/AppRoutes.test.tsx > redirect
    user from root to /fi by default
    Matched leaf route at location "/fi" does not have an element or
    Component. This means it will render an <Outlet /> with a null value by
    default resulting in an "empty" page.
    
    stderr | src/domain/app/routes/__tests__/AppRoutes.test.tsx > user from
    supported locale will be redirect to LocaleRoutes with that locale
    Matched leaf route at location "/en/" does not have an element or
    Component. This means it will render an <Outlet /> with a null value by
    default resulting in an "empty" page.
    ```
    
    refs PT-1807

--- src/domain/app/router/constants.ts
+++ src/domain/app/router/constants.ts
@@ -2,6 +2,7 @@ import type { BrowserRouterProps, MemoryRouterProps } from 'react-router-dom';
 
 export const DEFAULT_ROUTER_PROPS = {
   future: {
+    v7_relativeSplatPath: true,
     v7_startTransition: true,
   },
 } as const satisfies BrowserRouterProps satisfies MemoryRouterProps;
