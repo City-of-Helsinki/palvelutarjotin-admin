f8dcd1b2c9a964c1c38755ad7927e4f3f24b1ea4
Mikko Riippi <mikko.riippi@gofore.com>
2026-05-08 16:17:09 +0300

    fix: resolve html-to-draftjs CJS/ESM interop error with Vite 8
    Refs: PT-2037

--- src/common/components/textEditor/TextEditor.tsx
+++ src/common/components/textEditor/TextEditor.tsx
@@ -14,7 +14,7 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import classNames from 'classnames';
 import { ContentState, convertToRaw, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
-import htmlToDraft from 'html-to-draftjs';
+import htmlToDraftImport from 'html-to-draftjs';
 import React from 'react';
 import { Editor, EditorState as EditorStateWysiwyg } from 'react-draft-wysiwyg';
 import { useTranslation } from 'react-i18next';
@@ -23,6 +23,13 @@ import { getTextEditorLocalization, toolbarOptions } from './constants';
 import styles from './textEditor.module.scss';
 import useIsMounted from '../../../hooks/useIsMounted';
 import InputWrapper, { InputWrapperProps } from '../inputWrapper/InputWrapper';
+
+// Vite 8 CJS/ESM interop: html-to-draftjs uses module.exports, unwrap .default if needed
+const htmlToDraft: typeof htmlToDraftImport =
+  typeof htmlToDraftImport === 'function'
+    ? htmlToDraftImport
+    : (htmlToDraftImport as unknown as { default: typeof htmlToDraftImport })
+        .default;
 
 const convertHtmlToEditorState = (html: string) => {
   const blocksFromHtml = htmlToDraft(html);
